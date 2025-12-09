489b86762cefe3694b629fc54763e4e5324978aa
Soph <ceciliaknaub@cecilias-air.mynetworksettings.com>
2025-12-08 19:17:24 -0500

    add petals to grid

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,6 @@
 let list_item = document.querySelector('.hover-text');
 let img_1 = document.querySelector('img');
 
-
 list_item.addEventListener("mouseover", function(){
     img_1.src = "./assets/design.jpeg"
     list_item.style.cursor = 'default'
